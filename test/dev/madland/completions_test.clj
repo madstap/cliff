62c0d587dc57099ce1db13f2efbb8fa01c9ad7ee
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2021-07-26 23:49:04 -0300

    Add some failing tests

--- test/dev/madland/completions_test.clj
+++ test/dev/madland/completions_test.clj
@@ -1,5 +1,5 @@
 (ns dev.madland.completions-test
-  (:require [clojure.test :refer [deftest testing is are]]
+  (:require [clojure.test :refer [deftest testing is]]
             [dev.madland.cliff :as cliff]
             [clojure.string :as str]))
 
@@ -18,6 +18,7 @@
                  ["prog" {:opts [[nil "--foo"]]}
                   ["foo" {:handler identity}]
                   ["bar" {:handler identity}]]))))
+
   (testing "When word is --, suggest options"
     (testing "without arguments"
       (is (= [{:candidate "--foo"
@@ -32,4 +33,38 @@
              (cmps "prog --"
                    ["prog" {:opts [[nil "--foo FOO"]]}
                     ["foo" {:handler identity}]
-                    ["bar" {:handler identity}]]))))))
+                    ["bar" {:handler identity}]])))))
+
+  (testing "when word is a long opt with an arg"
+
+    (testing "with trailing ="
+      (is (= [{:candidate "foo"
+               :on-complete :next}
+              {:candidate "bar"
+               :on-complete :next}]
+             (cmps "prog --foo="
+                   ["prog" {:opts [[nil "--foo FOO"
+                                    :type :enum
+                                    :values [:foo :bar]]]}])))
+      (is (= [{:candidate "foo"
+               :on-complete :next}]
+             (cmps "prog --foo=f"
+                   ["prog" {:opts [[nil "--foo FOO"
+                                    :type :enum
+                                    :values [:foo :bar]]]}]))))
+
+    (testing "next word"
+      (is (= [{:candidate "foo"
+               :on-complete :next}
+              {:candidate "bar"
+               :on-complete :next}]
+             (cmps "prog --foo "
+                   ["prog" {:opts [[nil "--foo FOO"
+                                    :type :enum
+                                    :values [:foo :bar]]]}])))
+      (is (= [{:candidate "foo"
+               :on-complete :next}]
+             (cmps "prog --foo f"
+                   ["prog" {:opts [[nil "--foo FOO"
+                                    :type :enum
+                                    :values [:foo :bar]]]}]))))))
